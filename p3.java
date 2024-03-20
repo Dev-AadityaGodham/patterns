ecc52a99d648bfb6db3c530af1976ac19ee9facf
Dev-AadityaGodham <159020029+Dev-AadityaGodham@users.noreply.github.com>
2024-03-20 20:45:29 +0530

    Create p3.java

---
+++ p3.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ pattern 
+ *
+ **
+ ***
+ ****
+ ***** 
+*******************************************************************************/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 6;
+		for(int row =1;row<=n;row++){
+		    for(int j=1;j<=row;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
