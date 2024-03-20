3ce3ed2af74210a2569a6c5e199fc1fb1a7f4090
Dev-AadityaGodham <159020029+Dev-AadityaGodham@users.noreply.github.com>
2024-03-20 20:47:15 +0530

    Create p4.java

---
+++ p4.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ pattern 
+******
+*****
+****
+***
+**
+*
+*******************************************************************************/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 6;
+		for(int row =1;row<=n;row++){
+		    for(int j=row;j<=6;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
